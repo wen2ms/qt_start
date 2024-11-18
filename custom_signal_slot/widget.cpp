9ecfe667f1ec5905059233262f1456454eb0b5d2
wen2ms <1819509111@qq.com>
2024-11-18 22:10:33 +0800

    Feat: signals connect to signals

--- custom_signal_slot/widget.cpp
+++ custom_signal_slot/widget.cpp
@@ -1,4 +1,5 @@
 #include "widget.h"
+#include <QPushButton>
 
 Widget::Widget(QWidget *parent) : QWidget(parent), dog_(new Dog(this)), master_(new Master(this)) {
     void (Master::* color_ring)(QString) = &Master::ring_the_bell;
@@ -7,6 +8,25 @@ Widget::Widget(QWidget *parent) : QWidget(parent), dog_(new Dog(this)), master_(
     connect(master_, color_ring, dog_, color_stick);
     
     time_6_oclock("red");
+    
+    QPushButton* two_steps_ring_btn = new QPushButton("two_steps_ring_btn", this);
+    
+    void (Widget::* color_time)(void) =  &Widget::time_6_oclock;
+    
+    void (Master::* void_ring)(void) = &Master::ring_the_bell;
+    void (Dog::* void_stick)(void) = &Dog::stick_out_tough;
+    
+    connect(master_, void_ring, dog_, void_stick);
+    
+    connect(two_steps_ring_btn, &QPushButton::clicked, this, color_time);
+    
+    QPushButton* one_tap_ring_btn = new QPushButton("one_tap_ring", this);
+    
+    one_tap_ring_btn->move(200, 200);
+    
+    connect(one_tap_ring_btn, &QPushButton::clicked, master_, void_ring);
+    
+    this->resize(600, 400);
     
     setWindowTitle("Custom signal and slot");
 }

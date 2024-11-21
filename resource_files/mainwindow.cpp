14d0d78fde4e948a7edf5bb8bc5143635a1f2d6a
wen2ms <1819509111@qq.com>
2024-11-21 21:10:31 +0800

    Feat: modal and non-modal dialog

--- resource_files/mainwindow.cpp
+++ resource_files/mainwindow.cpp
@@ -11,6 +11,30 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     status_label->setText("status");
     
     ui->statusBar->addPermanentWidget(status_label);
+    
+    connect(ui->actionNew_Project, &QAction::triggered, this, [=]() {
+        QDialog modal_dialog(this);
+        
+        modal_dialog.setWindowTitle("Modal Dialog");
+        
+        modal_dialog.resize(200, 100);
+        
+        modal_dialog.exec(); 
+    });
+    
+    connect(ui->actionNew_File, &QAction::triggered, this, [=]() {
+        QDialog* non_modal_dialog = new QDialog(this);
+        
+        non_modal_dialog->setWindowTitle("Non-modal Dialog");
+        
+        non_modal_dialog->resize(200, 100);
+        
+        non_modal_dialog->setAttribute(Qt::WA_DeleteOnClose);
+        
+        non_modal_dialog->show();
+    });
+    
+    
 }
 
 MainWindow::~MainWindow() {
--- resource_files/mainwindow.h
+++ resource_files/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QLabel>
+#include <QDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {

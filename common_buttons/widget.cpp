9b41316e33f969920ecdc1c943bb5ec12074286a
wen2ms <1819509111@qq.com>
2024-11-27 16:04:19 +0800

    Feat: list widget

--- common_buttons/widget.cpp
+++ common_buttons/widget.cpp
@@ -12,6 +12,11 @@ Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget) {
     connect(ui->enabel_button, &QRadioButton::toggled, this, [=](bool enable_state) {
         qDebug() << "Enable state:" << enable_state;
     });
+    
+    QListWidgetItem* list_item = new QListWidgetItem("First Line", ui->listWidget);
+    list_item->setTextAlignment(Qt::AlignHCenter);
+    
+    ui->listWidget->addItem(list_item);
 }
 
 Widget::~Widget() {
--- common_buttons/widget.h
+++ common_buttons/widget.h
@@ -3,6 +3,7 @@
 
 #include <QDebug>
 #include <QWidget>
+#include <QListWidgetItem>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
